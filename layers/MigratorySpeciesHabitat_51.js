18bc979388a58ebaf6a04438779f5804456c2eac
iucngisdataportalmz <iucngisdataportal@gmail.com>
2025-09-19 20:52:23 +0200

    Add files via upload

---
+++ layers/MigratorySpeciesHabitat_51.js
@@ -0,0 +1,1 @@
+var json_MigratorySpeciesHabitat_51 = {"type":"FeatureCollection","name":"MigratorySpeciesHabitat_51","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
