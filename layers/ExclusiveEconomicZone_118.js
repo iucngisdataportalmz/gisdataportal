42a4d50699f48ea16b224915e05c488f45aa84d1
iucngisdataportalmz <iucngisdataportal@gmail.com>
2025-09-19 20:46:43 +0200

    Add files via upload

---
+++ layers/ExclusiveEconomicZone_118.js
@@ -0,0 +1,1 @@
+var json_ExclusiveEconomicZone_118 = {"type":"FeatureCollection","name":"ExclusiveEconomicZone_118","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
