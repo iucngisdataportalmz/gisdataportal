c46a8af4244dabaa95132d37843295e0a07c653b
iucngisdataportalmz <iucngisdataportal@gmail.com>
2025-09-19 20:43:32 +0200

    Add files via upload

---
+++ layers/Concessoesflorestais_109.js
@@ -0,0 +1,1 @@
+var json_Concessoesflorestais_109 = {"type":"FeatureCollection","name":"Concessoesflorestais_109","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
